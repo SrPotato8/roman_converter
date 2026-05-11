99559fdc6f59a36fbce46684826b640b89a939bd
SrPotato8 <132345100+SrPotato8@users.noreply.github.com>
2026-05-11 02:25:52 +0200

    Fix string quotes in toRoman function

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function toggleMode() {
 }
 
 function toRoman(num) {
-    if (isNaN(num) || num <= 0 || num > 3999) return "Invalid (1-3999)";
+    if (isNaN(num) || num <= 0 || num > 3999) return 'Invalid (1-3999)';
     const lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
     let roman = '';
     for (let i in lookup) {
@@ -35,7 +35,7 @@ function toDecimal(roman) {
             decimal += cur;
         }
     }
-    return isNaN(decimal) ? "Invalid Roman" : decimal;
+    return isNaN(decimal) ? 'Invalid Roman' : decimal;
 }
 
 function convertir() {
